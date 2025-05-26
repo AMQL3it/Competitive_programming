47c490b3de651f3012f114bd8db2b1d1308dd315
AMQL3it <atiqurrahmanrasel19@gmail.com>
2025-05-26 10:39:47 +0600

    Code update for tree

--- Graph_theory/building_tree.cpp
+++ Graph_theory/building_tree.cpp
@@ -211,19 +211,140 @@ void print_postorder(Node root) {
     cout << root->data << " ";
 }
 
-// Level-order Traversal
+// Level-order / Breadth-first Traversal
 void print_levelorder(Node root) {
     if (!root) return;
     queue<Node> q;
     q.push(root);
+    // q.push(NULL);
     while (!q.empty()) {
         Node curr = q.front();
         q.pop();
+        // if (!curr) {
+        //     cout << endl;
+        //     if (!q.empty()) q.push(NULL);
+        //     continue;
+        // }
         cout << curr->data << " ";
         if (curr->left) q.push(curr->left);
         if (curr->right) q.push(curr->right);
     }
 }
+
+//----------------------------- Basic calculations -----------------------------
+/*
+
+- Number of edges: Number of edges in the tree.
+- Number of nodes: Number of nodes in the tree.
+- Count of nodes: Number of nodes in the tree.
+- Sum of nodes: Sum of values of all nodes in the tree.
+- Count of leaves: Number of leaf nodes in the tree.
+- Depth of a node: Distance (number of edges) from the root to the node.
+- Height of a node: Maximum distance from the node to any leaf in its subtree.
+- Diameter of a tree: Maximum distance between any two nodes in the tree.
+
+- Size of a subtree: Number of nodes in the subtree.
+- Children of a node: Number of children of the node.
+- Siblings of a node: Number of siblings of the node.
+- Cousins of a node: Number of cousins of the node.
+
+
+
+*/
+
+//----------------------------- Count of nodes -----------------------------
+ll count_nodes(Node root) {
+    if (!root) return 0;
+    ll left = count_nodes(root->left);
+    ll right = count_nodes(root->right);
+    return 1 + left + right;
+}
+
+//----------------------------- Sum of nodes -------------------------------
+ll sum_of_nodes(Node root) {
+    if (!root) return 0;
+    ll left = sum_of_nodes(root->left);
+    ll right = sum_of_nodes(root->right);
+    return root->data + left + right;
+}
+
+//----------------------------- Count of leaves -----------------------------
+ll count_leaves(Node root) {
+    if (!root) return 0;
+    if (!root->left && !root->right) return 1;
+    ll left = count_leaves(root->left);
+    ll right = count_leaves(root->right);
+    return left + right;
+}
+
+//----------------------------- Depth of a node -----------------------------
+ll depth(Node root, int target) {
+    if (!root) return 0;
+    if (root->data == target) return 0;
+    ll left = depth(root->left, target);
+    ll right = depth(root->right, target);
+    return 1 + max(left, right);
+}
+
+//----------------------------- Height of a node -----------------------------
+ll height(Node root) {
+    if (!root) return 0;
+    ll left = height(root->left);
+    ll right = height(root->right);
+    return 1 + max(left, right);
+}
+
+
+//----------------------------- Diameter of Tree -----------------------------
+int diameter(Node root) {   // Time Complexity: O(N^2)
+    if (!root) return 0;
+    int left_height = height(root->left);
+    int right_height = height(root->right);
+    int left_diameter = diameter(root->left);
+    int right_diameter = diameter(root->right);
+    return max(1 + left_height + right_height, max(left_diameter, right_diameter));
+}
+
+struct TreeInfo {
+    int height;
+    int diameter;
+    TreeInfo(int h, int d) {
+        height = h;
+        diameter = d;
+    }
+};
+
+TreeInfo diameter_fast(Node root) {   // Time Complexity: O(N)
+    if (!root) return TreeInfo(0, 0);
+    TreeInfo left = diameter_fast(root->left);
+    TreeInfo right = diameter_fast(root->right);
+    int my_height = 1 + max(left.height, right.height);
+    int my_diameter = max(1 + left.height + right.height, max(left.diameter, right.diameter));
+    return TreeInfo(my_height, my_diameter);
+}
+
+// Lowest Common Ancestor (LCA)
+Node lca(Node root, int n1, int n2) {
+    if (!root) return NULL;
+    if (root->data == n1 || root->data == n2) return root;
+    Node left = lca(root->left, n1, n2);
+    Node right = lca(root->right, n1, n2);
+    if (left && right) return root;
+    return left ? left : right;
+}
+
+// Path from Root to Node
+vector<int> path_from_root(Node root, int target) {
+    vector<int> path;
+    while (root && root->data != target) {
+        path.push_back(root->data);
+        if (root->data > target) root = root->left;
+        else root = root->right;
+    }
+    path.push_back(root->data);
+    return path;
+}
+
 
 ///############################# Main Function ###############################
 
@@ -261,11 +382,21 @@ void DenTofieM() {
     int post_idx = postorder.size() - 1;
     Node root = build_tree_from_post_in(postorder, inorder, 0, inorder.size() - 1, post_idx, inorder_map);
 
+    cout << "Depth: " << depth(root) << nl;
+    cout << "Height: " << height(root) << nl;
+    cout << "Nodes: " << count_nodes(root) << nl;
+    cout << "Leaves: " << count_leaves(root) << nl;
+    cout << "Diameter: " << diameter(root) << nl;
+    cout << "LCA: " << lca(root, 4, 3)->data << nl;
+    // cout << "Path from Root to Node: ";
+    // for (int i : path_from_root(root, 4)) cout << i << " ";
+    // cout << nl;
+
     // Output Traversals
-    cout << "Preorder: "; print_preorder(root); cout << nl;
-    cout << "Inorder: "; print_inorder(root); cout << nl;
-    cout << "Postorder: "; print_postorder(root); cout << nl;
-    cout << "Levelorder: "; print_levelorder(root); cout << nl;
+    // cout << "Preorder: "; print_preorder(root); cout << nl;
+    // cout << "Inorder: "; print_inorder(root); cout << nl;
+    // cout << "Postorder: "; print_postorder(root); cout << nl;
+    // cout << "Levelorder: \n"; print_levelorder(root); cout << nl;
 }
 
 int main() {
